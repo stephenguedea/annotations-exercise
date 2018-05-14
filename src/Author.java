d818fc36f1b233fc1db08ebe5fa8780dffc2cbfc
Ryan Orsinger <ryan.orsinger@gmail.com>
2018-05-14 15:54:32 -0500

    make a better fix by resolving the arrayList of Strings, changed the name of sortName to fullName so it's actually overriding, and removed the deprecated method

--- src/Author.java
+++ src/Author.java
@@ -5,20 +5,13 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Author extends Person {
-    private List books;
+    private List<String> books;
 
     public Author(String firstName, String lastName) {
         super(firstName, lastName);
-        books = new ArrayList();
+        books = new ArrayList<String>();
     }
 
-    /**
-     * @deprecated Use publishedBooks instead
-     */
-    @Deprecated
-    public List<String> getBooks() {
-        return books;
-    }
 
     public List<String> publishedBooks() {
         return books;
@@ -29,7 +22,7 @@ public class Author extends Person {
     }
 
     @Override
-    public String sortName() {
+    public String fullName() {
         return String.format("%s, %s", lastName, firstName);
     }
 }
